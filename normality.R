3ce58e975c9f06cbcb64a02599c41d0636d94e71
abatsidis <79378345+abatsidis@users.noreply.github.com>
2022-04-11 11:54:14 +0300

    Add files via upload

---
+++ normality.R
@@ -0,0 +1,58 @@
+rm(list=ls())
+library(datasets)
+data<-data("iris")
+str(iris)
+
+library(moments)
+sepallength <- iris$Sepal.Length
+summary(sepallength)
+moments::skewness(sepallength)
+moments::kurtosis(sepallength)
+
+
+hist(sepallength)
+# fitted normal density
+f.den <- function(t) dnorm(t,mean(sepallength),sqrt(var(sepallength)))
+curve(f.den,xlim=c(4,8))
+hist(sepallength,prob=T,add=T)
+par (mfrow=c (1 ,2) )
+boxplot (sepallength, xlab ="  " , ylab =" Sepal Length " )
+
+library(rcompanion)
+plotNormalHistogram( sepallength, prob = FALSE,
+                      main = "Normal Distribution overlay on Histogram",
+                      length = 1000 )
+
+
+hist(sepallength, freq = FALSE)
+curve(dnorm(x, mean = mean(sepallength), sd =sd(sepallength)),
+      from = 3,
+      to = 9,
+      add = TRUE,
+     col = "red")
+
+
+library(e1071)
+# p-plot: you should observe a good fit of the straight line
+
+probplot(sepallength, qdist=qnorm)
+
+# qq-plot: you should observe a good fit of the straight line
+qqnorm(sepallength)
+qqline(sepallength)
+
+
+library(normtest)
+library(nortest)
+# Adjusted Jarque–Bera test for normality
+#The p-value is computed by Monte Carlo simulation 10000 runs
+ajb.norm.test(sepallength,10000)
+
+shapiro.test(sepallength)
+ad.test(sepallength)
+
+
+
+
+
+
