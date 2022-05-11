491f38731f4ac55236108735d7df7e41bc0afbaf
peconom <79377204+peconom@users.noreply.github.com>
2022-05-11 22:25:47 +0300

    Add files via upload

---
+++ TwoWayFirst.R
@@ -0,0 +1,34 @@
+ibrary(dplyr)
+# Normality test for each group
+ToothGrowth %>%
+  group_by(dose, supp) %>% 
+  summarise(n=n(),
+            statistic = shapiro.test(len)$statistic,
+            p.value = shapiro.test(len)$p.value)
+#Homogeneity of Variance
+library(car)
+leveneTest(len ~ dose*supp  , data = ToothGrowth)
+
+
+aov.res <- aov(len ~ dose + supp +  dose:supp, data = ToothGrowth)
+summary(aov.res)
+
+library(stats)
+with(ToothGrowth, {
+  interaction.plot(dose, supp, len, fixed = TRUE)
+  interaction.plot(dose, supp, len, fixed = TRUE, col = 2:3, leg.bty = "o")
+  interaction.plot(dose, supp, len, fixed = TRUE, col = 2:3, type = "b", main="Interaction plot")
+})
+
+
+library(lsmeans)
+Tuk<-lsmeans(aov.res, list(pairwise~dose|supp,pairwise~supp|dose,pairwise~dose+supp),  adjust="tukey")
+Tuk
+
+
+plot(Tuk[[1]])
+plot(Tuk[[2]])  
+plot(Tuk[[3]])  
+plot(Tuk[[4]])  
+plot(Tuk[[5]])  
+plot(Tuk[[6]]) 
