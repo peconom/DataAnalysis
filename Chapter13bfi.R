91f807244deb23389b52060751f0266c1bead945
abatsidis <79378345+abatsidis@users.noreply.github.com>
2022-05-29 11:33:41 +0300

    Update Chapter13bfi.R

--- Chapter13bfi.R
+++ Chapter13bfi.R
@@ -70,34 +70,3 @@ datanew1<- cbind(bfi,datascores)
 newdata<-bfi
 newdata[,c(1, 9,10,11,12,22,25)] <-7-newdata[,c(1, 9,10,11,12,22,25)] 
 
-library(psych)
-
-#get the data
-datafilename="http://personality-project.org/R/datasets/extraversion.items.txt" 
-mydata=read.table(datafilename,header=TRUE)  
-str(mydata)
-alpha<-psych::alpha(mydata[,2:6],check.keys=TRUE)
-alpha
-mydata.keys.list <- list(Extraversion=c(1, -2, 3, -4,5))
-mydata.keys <- make.keys(mydata[,2:6],mydata.keys.list,item.labels=colnames(mydata))
-mydata.scored <-scoreItems(mydata.keys, mydata[,2:6], impute="NULL", min=1, max=6, digits=3)
-
-library(psychTools)
-library(psych)
-str(epi)
-
-data("epi.dictionary")
-epi.dictionary
-epi.keys
-
-library(psychTools)
-library(psych)
-str(epi)
-
-data("epi.dictionary")
-epi.dictionary
-epi.keys
-Lie.dat<-data.frame(epi[,c(6,24,36,12,18,30,42,48,54)])
-alphaLie<-psych::alpha(Lie.dat,check.keys=TRUE)
-alphaLie
-
